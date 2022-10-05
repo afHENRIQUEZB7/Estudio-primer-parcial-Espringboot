9b305177c3bb7b750874838de8a1e69461934105
afHENRIQUEZB7 <afhenriquezb@ufpso.edu.co>
2022-10-05 14:59:40 -0500

    Primera practica

---
+++ src/main/java/com/prueba_Parcial1/Estudio_primer_Parcial/EstudioPrimerParcialApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_Parcial1.Estudio_primer_Parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioPrimerParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudioPrimerParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba_Parcial1/Estudio_primer_Parcial/EstudioPrimerParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_Parcial1.Estudio_primer_Parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudioPrimerParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
